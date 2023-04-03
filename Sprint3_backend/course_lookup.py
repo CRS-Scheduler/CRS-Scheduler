5c94746b843a990f2d9403aa9c33940a06dcb4d7
eyronjuude <actanael@up.edu.ph>
2023-04-03 09:12:26 +0800

    Add course lookup generator and test files

--- Sprint3_backend/course_lookup.py
+++ Sprint3_backend/course_lookup.py
@@ -1,1 +1,29 @@
-(['KAS 1', 'PHILO 1', 'MATH 21', 'CS 11', 'CS 30', 'CS 10', 'PE 1', 'PE 2'], ['SOC SCI 1', 'SOC SCI 3', 'SOC SCI 2', 'MATH 23'], ['MIDYEAR SEM', 'COURSES'])
+import sys
+# from pathlib import PurePath 
+from pathlib import Path
+
+def course_lister(root):
+    obj = dict()
+    # p = Path(".")
+    # degree_programs = filter(lambda x: x.is_dir(), p.iterdir())
+    # year_levels = map(lambda x: (x, sorted(x.glob("*.txt"))), degree_programs)
+    # for (root, txtfilepath) in year_levels:
+    txtfilepath = sorted(Path(root).glob("*.txt"))
+    obj[root] = []
+    for txtfilename in txtfilepath:
+        obj[root].append(write_lookup(txtfilename))
+    return obj
+
+def write_lookup(txtfilename):
+    file = open(txtfilename, "r+")
+    data = tuple(file.read().split("\n\n"))
+    write = list(map(lambda x: x.split("\n"), data))
+    return write
+
+ret = course_lister("BS-CS")
+# python = open(f"course_lookup.py", "w")
+# python.seek(0)
+# python.truncate()
+# python.write(str(ret))
+print(ret)
+sys.exit()
