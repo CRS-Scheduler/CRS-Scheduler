85829d04c11d33f76765bfe305cfab72ea3f717a
Josh <amadorjoshua989@gmai.com>
2023-04-28 01:38:33 +0800

    Added jsonparser.py to parse txt tree to JSON format.

---
+++ Sprint3_backend/jsonparser.py
@@ -0,0 +1,17 @@
+import os, json
+progs = dict()
+
+for (root,dirs,files) in os.walk('./DPrograms', topdown=True):
+    if root == './DPrograms':
+        continue
+    else:
+        mroot = root.split("\\")[1]
+    progs[mroot] = dict()
+    print(root, files)
+    for f in files:
+        progs[mroot][f.rstrip(".txt")] = "\n".join(open((os.getcwd()+root[1:]+"/"+f).replace("\\", "/"), "r").readlines()).split("\n")
+        progs[mroot][f.rstrip(".txt")] = list(set(progs[mroot][f.rstrip(".txt")]))
+
+js = open("dprograms.json", "w")
+js.write(json.dumps(progs))
+js.close()
