bb8d204d33033cfad599e7408bf1ff376de3f990
eyronjuude <actanael@up.edu.ph>
2023-04-28 00:54:25 +0800

    fix: encoding of courses by adding spaces for blank `midyear_sem`s

--- Sprint3_backend/CourseValidator.py
+++ Sprint3_backend/CourseValidator.py
@@ -159,7 +159,7 @@ def get_course_list_from_program(program: str): #get_course_list_from_program(bs
     for year in deg.courses:
         for sem in year:
             flat.extend(sem)
-    return list(set(flat))
+    print(list(set(flat)))
 
 def main():
     pass
