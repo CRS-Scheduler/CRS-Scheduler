a336c3849a49f76db73574d5d478f2956f758898
eyronjuude <actanael@up.edu.ph>
2023-05-10 15:11:43 +0800

    refactor: change `import datetime` to `from datetime import time`

--- Sprint3_backend/main.py
+++ Sprint3_backend/main.py
@@ -1,14 +1,14 @@
-import datetime
+from datetime import time
 from schedule import Schedule
 
 # Create a new schedule object
 my_schedule = Schedule()
 
 # Set CS21 on Monday from 7:00 to 8:30
-my_schedule.set_event('Monday', datetime.time(7, 0), datetime.time(8, 30), 'CS21')
+my_schedule.set_event('Monday', time(7, 0), time(8, 30), 'CS21')
 # Set CS138 on Tuesday, Thursday from 15:00 to 16:30
-my_schedule.set_event('Tuesday', datetime.time(15, 0), datetime.time(16, 30), 'CS138')
-my_schedule.set_event('Thursday', datetime.time(15, 0), datetime.time(16, 30), 'CS138')
+my_schedule.set_event('Tuesday', time(15, 0), time(16, 30), 'CS138')
+my_schedule.set_event('Thursday', time(15, 0), time(16, 30), 'CS138')
 
 # Print current schedule
 my_schedule.print_schedule();
