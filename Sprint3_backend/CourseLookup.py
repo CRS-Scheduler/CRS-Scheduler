63401d7eeb8a93d444379ec06af3661e80f450eb
eyronjuude <actanael@up.edu.ph>
2023-05-12 13:51:51 +0800

    refactor: simplify `course_lister` to contain curriculum parsing

--- Sprint3_backend/CourseLookup.py
+++ Sprint3_backend/CourseLookup.py
@@ -1,15 +1,7 @@
 from pathlib import Path
 
 def course_lister(root, year, semester):
-    # p = Path(".")
-    # degree_programs = filter(lambda x: x.is_dir(), p.iterdir())
-    # year_levels = map(lambda x: (x, sorted(x.glob("*.txt"))), degree_programs)
-    # for (root, txtfilepath) in year_levels:
-    txtfilepath = sorted(Path(root).glob(f"{root}_{year}.txt"))
-    return write_lookup(txtfilepath[0], semester-1)
-
-def write_lookup(txtfilename, semester):
-    file = open(txtfilename, "r+")
-    data = tuple(file.read().split("\n\n"))
-    write = list(map(lambda x: x.split("\n"), data))
-    return write[semester]
+    file = open(f"{root}/{root}_{year}.txt", "r+")
+    data = list(file.read().split("\n\n"))
+    write = list(map(lambda x: x.split("\n"), data))[semester-1]
+    return write
