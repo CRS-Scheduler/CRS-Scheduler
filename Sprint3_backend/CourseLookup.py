bae1977ebb6dfdca8af033622d72b9faa9baacf9
eyronjuude <actanael@up.edu.ph>
2023-05-12 14:22:43 +0800

    feat: add `course_parser` function for parseable courses

--- Sprint3_backend/CourseLookup.py
+++ Sprint3_backend/CourseLookup.py
@@ -1,7 +1,17 @@
+import random
 from pathlib import Path
 
 def course_lister(root, year, semester):
     file = open(f"{root}/{root}_{year}.txt", "r+")
-    data = list(file.read().split("\n\n"))
+    data = file.read().split("\n\n")
     write = list(map(lambda x: x.split("\n"), data))[semester-1]
-    return write
+    return course_parser(root, year, write)
+
+def course_parser(root, year, course_list):
+    for i in range(0,len(course_list)):
+        course = course_list[i]
+        if course[0:len(root)] == root:
+            extras_file = open(f"{root}/{root}{course[len(root):]}.txt", "r+")
+            course_extras = extras_file.read().split("\n")
+            course_list[i] = random.choice(course_extras)
+    return course_list
