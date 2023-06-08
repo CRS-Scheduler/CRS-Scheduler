18cba110e4800f713a7091b1a20f375a5dfa2016
Koniiro <81352867+Koniiro@users.noreply.github.com>
2023-06-09 00:21:37 +0800

    cors issue fix

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request
 import ScheduleOptimizer, json, Scheduler2
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
