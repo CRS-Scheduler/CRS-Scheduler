cb4a3a07182e1a2d6390572d637976640032e47f
Koniiro <81352867+Koniiro@users.noreply.github.com>
2023-06-08 21:00:52 +0800

    some updates and typos

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import ScheduleOptimizer, json
+import ScheduleOptimizer, json, Scheduler2
 app = Flask(__name__)
 
 @app.route('/')
--- lib/screens/schedulescreen.dart
+++ lib/screens/schedulescreen.dart
@@ -70,7 +70,7 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
   fetchSched(String course, int yr) async {
 // Define the API endpoint URL
     final String apiUrl = 'http://127.0.0.1:5000/api/schedule?prgm=$course&yrlvl=$yr&seed=1';
-    //final String apiUrl = 'https://crs-service-192.onrender.com/api/schedule?prgm=$course&yrlvl=$yr';
+    //final String apiUrl = 'https://crs-service-192.onrender.com/api/schedule?prgm=$course&yrlvl=$yr&seed=1';
     if (kDebugMode) {
       print(apiUrl);
     }
