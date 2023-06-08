ed990834b47f42fdc36ccf65e88d93c6ce58b349
Koniiro <81352867+Koniiro@users.noreply.github.com>
2023-06-08 22:07:25 +0800

    some updates

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,8 +26,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "bf79bf962d756aec177b7b0c9241a84d
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "dde1531ff106150821e9df5c9a3e9918",
-"/": "dde1531ff106150821e9df5c9a3e9918",
+"index.html": "dc3ce590ac83cdd2dc11b318da0dcff2",
+"/": "dc3ce590ac83cdd2dc11b318da0dcff2",
 "main.dart.js": "b12f44a390aef03cacc1d72a35e323a6",
 "manifest.json": "ca3199ec0a24044111c0c39677a9cb74",
 "version.json": "4ec30b6de56f6781aa67d4effe39b885"};
